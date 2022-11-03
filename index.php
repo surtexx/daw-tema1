1b338b4f297de55902ab624f13ae84d7e3e7b93a
surtexx <91895868+surtexx@users.noreply.github.com>
2022-11-03 11:07:18 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head lang="en">
+	<title>Tema1</title>
+</head>
+<body>
+<?php
+	echo '<h1>Proiectul va fi realizat de Gheorghe Robert-Mihai (grupa 243).</h1>';
+	echo '<p>Pentru acest proiect mi-am ales tema <b>Evidenta rezultatelor concursurilor sportive</b> deoarece sunt fan sporturi.<br>
+	Proiectul consta intr-o aplicatie web ce va afisa rezultatele partidelor dintr-o anumita competitie dintr-un anumit sport, clasamente ale competitiilor pe echipe, cat si individuale,<br>
+	precum si statistici importante despre acestea. Pentru administratori va exista un meniu de tip CRUD ce va permite adaugarea de noi competitii si partide, citirea unor competitii sau partide existente,<br>
+	actualizarea acestora, precum si stergerea. Pentru utilizatori, interfata grafica ca contine multe informatii despre toate competitiile din baza de date, precum si o optiune de citire din baza de date<br>
+	a unei competitii sau partida existenta.<br></p>';
+	echo '<p>Interfata grafica va fi realizata folosind HTML si CSS pentru design, iar back-end-ul va folosi elemente de PHP.<br>
+	Va exista o conexiune cu o baza de date MySQL pentru facilitarea mai rapida a cererilor informationale.</p><br>';
+	echo '<img src="diagrama.png" alt="diagrama">';
+?>
+</body>
+</html>
